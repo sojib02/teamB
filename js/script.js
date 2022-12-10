f892114f95862285e80e7868b76eabfca52bfdc9
Sojib <shojibmia626@yahoo.com>
2022-12-10 21:29:13 +0600

    teamB

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(function(){
+    $('.single-block-text').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.single-block-media'
+      });
+      $('.single-block-media').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        asNavFor: '.single-block-text',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        centerPadding: '0px',
+        arrows: false,
+        autoplay: true
 
+      });
+      
 })
