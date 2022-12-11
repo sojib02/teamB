dd01ef695f1373d1113d6c73140ec74f60229689
Sojib <shojibmia626@yahoo.com>
2022-12-11 21:49:26 +0600

    teamB

--- js/script.js
+++ js/script.js
@@ -39,6 +39,31 @@ $(function(){
       arrows: false,
       autoplay: true,
       autoplaySpeed: 1000,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     });
     // CLIENT PART END 
 
@@ -54,6 +79,31 @@ $(function(){
         focusOnSelect:true,
         centerMode: true,
         centerPadding: '0px',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
     // APP SLIDER PART END 
 })
